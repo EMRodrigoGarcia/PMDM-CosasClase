9eef1c9d023f556ca97c9a80713547feb67a1f5a
Rodrigo G <rglopez300@gmail.com>
2020-10-28 17:22:57 +0100

    Prueba commit

--- app/src/main/java/com/example/myapplication/PruebaXML.java
+++ app/src/main/java/com/example/myapplication/PruebaXML.java
@@ -1,12 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.ButtonBarLayout;
 
-import android.graphics.Color;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class PruebaXML extends AppCompatActivity {
@@ -16,24 +13,21 @@ public class PruebaXML extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_x_m_l);
 
-        Button b = findViewById(R.id.button);
-        t = findViewById(R.id.textView);
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int c = Color.BLUE;
-                double rand = Math.random();
-                if (rand < 0.25) {
-                   c = Color.RED;
-                }else if (rand > 0.25 && rand < 0.5) {
-                    c = Color.YELLOW;
-                }else if(rand > 0.5 && rand < 0.75) {
-                    c = Color.GREEN;
-                }
-
-                t.setTextColor(c);
-            }
-        });
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_prueba_x_m_lh);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            setContentView(R.layout.activity_prueba_x_m_l);
+        }
+    }
+
+
+
 }
