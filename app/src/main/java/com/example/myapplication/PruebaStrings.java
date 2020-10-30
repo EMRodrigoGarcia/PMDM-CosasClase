6d6c2f40eb7ed58d9a39474bad307a75e9a80ceb
Rodrigo G <rglopez300@gmail.com>
2020-10-30 19:56:34 +0100

    Prueba commit

--- app/src/main/java/com/example/myapplication/PruebaStrings.java
+++ app/src/main/java/com/example/myapplication/PruebaStrings.java
@@ -2,18 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class PruebaXML extends AppCompatActivity {
-    TextView t;
+public class PruebaStrings extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prueba_x_m_l);
-
-
+        setContentView(R.layout.activity_prueba_strings);
     }
-
 }
