1f428d3acfcd2d2df6dcebf0ed4c426618a18d28
Rodrigo García López <rglopez300@gmail.com>
2020-11-20 20:36:46 +0100

    TODO arreglar comunicacion entre frags / actualizado proyecto con viewmodel

--- app/src/main/java/com/example/myapplication/A1_Frag1.java
+++ app/src/main/java/com/example/myapplication/A1_Frag1.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,12 +38,12 @@ public class A1_Frag1 extends Fragment {
         tv = v.findViewById(R.id.textView5);
         return v;
     }
-
+    //TODO: Hacer que se comuniquen los dos fragmentos
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         viewModel = new ViewModelProvider(requireActivity()).get(A1_SharedViewModel.class);
-        tv.setTextColor(viewModel.getColor());
+        //tv.setTextColor(viewModel.getColor());
 
     }
 }
